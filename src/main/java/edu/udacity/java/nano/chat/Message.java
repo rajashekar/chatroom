6aa273b3e0e4d6130378ed3f147ef1a339d4c525
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2019-08-30 00:23:44 -0700

    feat: added login logic

---
+++ src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -0,0 +1,54 @@
+package edu.udacity.java.nano.chat;
+
+/**
+ * WebSocket message model
+ */
+public class Message {
+    // TODO: add message model.
+    private String type;
+    private String username;
+    private String msg;
+    private int onlineCount;
+
+    public Message() {
+    }
+
+    public Message(String type, String username, String msg, int onlineCount) {
+        this.type = type;
+        this.username = username;
+        this.msg = msg;
+        this.onlineCount = onlineCount;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMsg() {
+        return this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getOnlineCount() {
+        return this.onlineCount;
+    }
+
+    public void setOnlineCount(int onlineCount) {
+        this.onlineCount = onlineCount;
+    }
+}
